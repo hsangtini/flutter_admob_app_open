266e0bba4822ec5beda26346d8bcb18ecac61db8
RockyLeee <nthieu21990@gmail.com>
2020-10-31 15:03:23 +0700

    ...

--- ios/Classes/FlutterAdmobAppOpenPlugin.h
+++ ios/Classes/FlutterAdmobAppOpenPlugin.h
@@ -1,4 +1,13 @@
 #import <Flutter/Flutter.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
 
 @interface FlutterAdmobAppOpenPlugin : NSObject<FlutterPlugin>
+@property(strong, nonatomic) UIWindow* window;
+@property(nonatomic) GADAppOpenAd* appOpenAd;
+@property(weak, nonatomic) NSDate *loadTime;
+@property(weak, nonatomic) NSString *appId;
+@property(weak, nonatomic) NSString *appAppOpenAdUnitId;
+
+- (void)requestAppOpenAd;
+- (void)tryToPresentAd;
 @end
--- ios/Classes/FlutterAdmobAppOpenPlugin.m
+++ ios/Classes/FlutterAdmobAppOpenPlugin.m
@@ -12,4 +12,87 @@
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftFlutterAdmobAppOpenPlugin registerWithRegistrar:registrar];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS "
+                stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"initialize" isEqualToString:call.method]) {
+        NSString *appId = myArgs["appId"] as? String,
+        NSString *appAppOpenAdUnitId = myArgs["appAppOpenAdUnitId"] as? String,
+        self.appId = appId
+        self.appAppOpenAdUnitId = appAppOpenAdUnitId
+        if (appId == null || appId.isEmpty()) {
+            result(FlutterError(code: "no_app_id", message: "a null or empty AdMob appId was provided", details: nil))
+            return
+        }
+        [GADMobileAds configureWithApplicationID:appId];
+        result(true)
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [self tryToPresentAd];
+}
+
+- (void)requestAppOpenAd {
+  self.appOpenAd = nil;
+  [GADAppOpenAd loadWithAdUnitID:self.appAppOpenAdUnitId
+                         request:[GADRequest request]
+                     orientation:UIInterfaceOrientationPortrait
+               completionHandler:^(GADAppOpenAd *_Nullable appOpenAd, NSError *_Nullable error) {
+                 if (error) {
+                   NSLog(@"Failed to load app open ad: %@", error);
+                   return;
+                 }
+                 self.appOpenAd = appOpenAd;
+                 self.appOpenAd.fullScreenContentDelegate = self;
+                 self.loadTime = [NSDate date];
+               }];
+}
+
+#pragma mark - GADFullScreenContentDelegate
+
+/// Tells the delegate that the ad failed to present full screen content.
+- (void)ad:(nonnull id<GADFullScreenPresentingAd>)ad
+    didFailToPresentFullScreenContentWithError:(nonnull NSError *)error {
+  NSLog(@"didFailToPresentFullSCreenCContentWithError");
+  [self requestAppOpenAd];
+
+}
+
+/// Tells the delegate that the ad presented full screen content.
+- (void)adDidPresentFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+  NSLog(@"adDidPresentFullScreenContent");
+}
+
+/// Tells the delegate that the ad dismissed full screen content.
+- (void)adDidDismissFullScreenContent:(nonnull id<GADFullScreenPresentingAd>)ad {
+  NSLog(@"adDidDismissFullScreenContent");
+  [self requestAppOpenAd];
+}
+
+- (BOOL)wasLoadTimeLessThanNHoursAgo:(int)n {
+  NSDate *now = [NSDate date];
+  NSTimeInterval timeIntervalBetweenNowAndLoadTime = [now timeIntervalSinceDate:self.loadTime];
+  double secondsPerHour = 3600.0;
+  double intervalInHours = timeIntervalBetweenNowAndLoadTime / secondsPerHour;
+  return intervalInHours < n;
+}
+
+- (void)tryToPresentAd {
+  GADAppOpenAd *ad = self.appOpenAd;
+  self.appOpenAd = nil;
+  if (ad && [self wasLoadTimeLessThanNHoursAgo:4]) {
+    UIViewController *rootController = self.window.rootViewController;
+    [ad presentFromRootViewController:rootController];
+
+  } else {
+    // If you don't have an ad ready, request one.
+    [self requestAppOpenAd];
+  }
+}
+
 @end
